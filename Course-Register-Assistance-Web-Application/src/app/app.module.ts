9408188e5662410871a7872f15db9ce18f20f311
kobowon <ts3507@ajou.ac.kr>
2017-11-24 21:27:16 +0900

    20171124-고보원-searchList수정, mock.subjectList.ts추가, subject.ts 수정

--- Course-Register-Assistance-Web-Application/src/app/app.module.ts
+++ Course-Register-Assistance-Web-Application/src/app/app.module.ts
@@ -2,12 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { SugangLogin } from './sugangLogin.component';
-=======
->>>>>>> de402ab0bf67f8abab62dc264354890ff8a9f570
 
 import { HttpService } from './http-service';
 import { NotifyService } from './notify-service';
@@ -19,13 +14,9 @@ import {SearchListComponent} from './searchList.component';
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     SearchEngineComponent,
     SearchListComponent,
-    SugangLogin
-=======
     SearchEngineComponent
->>>>>>> de402ab0bf67f8abab62dc264354890ff8a9f570
   ],
   imports: [
     BrowserModule,
