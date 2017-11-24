c7eb31966a7a9fa0e0232ae6fa431468f655045e
kobowon <ts3507@ajou.ac.kr>
2017-11-24 12:53:33 +0900

    20171124-고보원-searchEngine.component수정

---
+++ Course-Register-Assistance-Web-Application/src/app/searchList.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-searchlist',
+  templateUrl: './searchEngine.component.html',
+  styleUrls: ['./searchEngine.component.css']
+})
+
+export class SearchListComponent {
+}
