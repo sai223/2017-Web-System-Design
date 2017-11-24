219cc7be826b042cc6e65a88c633734961fda17b
kobowon <ts3507@ajou.ac.kr>
2017-11-24 22:55:16 +0900

    20171124-고보원-searchList수정, mock.subjectList.ts추가, subject.ts 수정

--- Course-Register-Assistance-Web-Application/src/app/searchList.component.ts
+++ Course-Register-Assistance-Web-Application/src/app/searchList.component.ts
@@ -8,10 +8,15 @@ import {Subject} from './Subject';
 })
 
 export class SearchListComponent implements OnInit {
-  constructor() {
-  }
   @Input() searchList: Subject[] = [];
+  selectedSubject: Subject;
   ngOnInit() {
     console.log(this.searchList);
+    this.selectedSubject = new Subject();
+  }
+  // 표를 클릭하면 selectedSubject 에 클릭한 과목 넣어
+  choice(subject: Subject) {
+    this.selectedSubject = subject;
+    console.log('선택된 과목은 ', this.selectedSubject, '입니다')
   }
 }
