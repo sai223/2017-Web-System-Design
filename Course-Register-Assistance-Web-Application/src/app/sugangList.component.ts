627d9a1ae7d0d3bfac48207b9ab40d93321a7cde
hbigman0921 <hbigman0921@ajou.ac.kr>
2017-12-04 16:35:37 +0900

    20171204-이인태-수강 추가, 수강 삭제 기능추가2

--- Course-Register-Assistance-Web-Application/src/app/sugangList.component.ts
+++ Course-Register-Assistance-Web-Application/src/app/sugangList.component.ts
@@ -1,5 +1,6 @@
 import {Input, Component, ElementRef, AfterViewInit,
         Output, EventEmitter} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
 
 import {Sugang} from './Sugang';
 import {HttpService} from './http-service';
@@ -29,25 +30,34 @@ export class SugangListComponent implements AfterViewInit {
     this.elementRef.nativeElement.appendChild(s);
   }
   copyClick(no: number) {
-  copyServiceObject.copyFunc(no);
+    copyServiceObject.copyFunc(no);
+  }
+  addSugang() {
+    this.httpService.addSubject(true, this.currentSugangName, this.currentSugangNumber).subscribe(result => {
+  if (result == true) {
+  // app.component에 보낸다. 그러면 app.component에서 getAllSubject해서 업데이트 함.
+  this.currentSugangName = '';
+  this.currentSugangNumber = '';
+  this.updateSugangList.emit();
+}else {
+  // 안보내고, 오류 메세지 띄운다.
+  this.currentSugangNumber = '';
+  alert('과목 번호는 올바른 값을 입력해야 합니다.');
 }
-addSugang() {
-  this.httpService.addSubject(true, this.currentSugangName, this.currentSugangNumber).subscribe(result => {
-    if (result == true) {
-      // app.component에 보낸다. 그러면 app.component에서 getAllSubject해서 업데이트 함.
-      this.currentSugangName = '';
-      this.currentSugangNumber = '';
-      this.updateSugangList.emit('update');
-    }else {
-      // 안보내고, 오류 메세지 띄운다.
-      this.currentSugangNumber = '';
-      alert('과목 번호는 올바른 값을 입력해야 합니다.');
-    }
-  });
+});
 }
 deleteSugang(no: string) {
   this.httpService.deleteSubject(no).subscribe(result => {
-    this.updateSugangList.emit('update');
+    this.updateSugangList.emit();
   });
 }
+alarmInterval: any;
+alarmOn() {
+  this.alarmInterval = setInterval(() => {
+    alert('수강신청 5초 전입니다.');
+  }, 5000);
 }
+alarmOff() {
+  clearInterval(this.alarmInterval);
+}
+}// end of class SugangListComponent
