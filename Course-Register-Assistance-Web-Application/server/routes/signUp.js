2e5ac9cb08d978eb9569eb8622cda5d724b518a6
kihong <sai223@ajou.ac.kr>
2017-12-05 23:03:33 +0900

    20171205-김기홍-routes/signUp 추가: 처음

---
+++ Course-Register-Assistance-Web-Application/server/routes/signUp.js
@@ -0,0 +1,36 @@
+const express = require ('express');
+const router = express.Router();
+var mongoose = require('mongoose');
+const ClientInfo = require('../database/clientInfoModel');
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+
+router.post('/requestSignUp', function (req, res) {
+
+    ClientInfo.findOne({userID: req.body.signID}, function (err, info){
+      if (err) {
+        return console.log("err " + err);
+      }
+      if(info){ //ID가 있으면
+        console.log('아이디 중복: '+info.userName);
+        const isValid = {boolean: false};
+        res.send(isValid);
+      } else { //ID가 없으면
+        cliInfo = new ClientInfo();
+        cliInfo.userID = req.body.signID;
+        cliInfo.userPassword = req.body.signPW;
+        cliInfo.userName = req.body.signName;
+
+        cliInfo.save(function(err, document) {
+            if(err) {
+                return console.log("err " + err);
+            }
+            const cliInfo = {boolean: true};
+            res.send(isValid);
+            console.log('아이디 생성 완료: '+ document);
+        });
+      }
+    })
+});
