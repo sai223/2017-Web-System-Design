bcf4910d4e44a34b9795cdc91f10d9dce98da2e8
hbigman0921 <hbigman0921@ajou.ac.kr>
2017-12-10 17:31:54 +0900

    20171210-이인태-css작업

--- Course-Register-Assistance-Web-Application/src/app/app.component.ts
+++ Course-Register-Assistance-Web-Application/src/app/app.component.ts
@@ -16,6 +16,8 @@ export class AppComponent implements OnInit {
   @ViewChild('logOutTemplate') logOutTemplate: TemplateRef<any>;
   enrollList_T: Subject[] = [];
   sugangList: Sugang[] = [];
+  list_1_style: string;
+  list_2_style: string;
   currentPage: number; // 1: 수강신청 페이지, 2: 시간표 조회 페이지
   loginState: boolean; // true: 로그인 상태, false: 로그아웃 상태
   userID: string;
@@ -38,14 +40,22 @@ export class AppComponent implements OnInit {
         this.loginState = true;
 
         this.currentPage = JSON.parse(JSON.stringify(result)).page;
-
-        this.page1Active = true;
+        this.changeListCSS();
         this.changeTemplate();
         this.getAllSubject();
       } else {
         console.log('첫접속 브라우저');
       }
     });
+  }
+  changeListCSS() {
+    if (this.currentPage === 1) {
+      this.list_1_style = '.li_selected';
+      this.list_2_style = '.li_unselected';
+    }else if (this.currentPage === 2) {
+      this.list_1_style = '.li_unselected';
+      this.list_2_style = 'li_selected';
+    }
   }
   handleLogInOut(flag: boolean) {
     if (flag === true) { // logout -> login
@@ -94,9 +104,9 @@ export class AppComponent implements OnInit {
         this.currentTemplate = this.logInTimetableTemplate;
       } else {
         this.currentPage = 1;
+        this.changeListCSS();
         this.currentTemplate = this.logInSugangTemplate;
       }
-
     }
     this.currentView.destroy();
     console.log('change5');
@@ -136,23 +146,19 @@ export class AppComponent implements OnInit {
     }
     this.clearSignupModal();
   }
-  page1Active: boolean = false;
-  page2Active: boolean = false;
   changeCurrentPage(no: number) {
     if (this.currentPage === 1) {
       if (no !== 1) {
-        this.page1Active = true;
-        this.page2Active = false;
         this.currentPage = 2;
+        this.changeListCSS();
         this.httpService.pageSession(this.currentPage).subscribe();
         this.currentView.destroy();
         this.changeTemplate();
       }
     }else if (this.currentPage === 2) {
       if (no !== 2) {
-        this.page1Active = false;
-        this.page2Active = true;
         this.currentPage = 1;
+        this.changeListCSS();
         this.httpService.pageSession(this.currentPage).subscribe();
         this.currentView.destroy();
         this.changeTemplate();
