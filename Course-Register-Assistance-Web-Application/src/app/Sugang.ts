3cd700185e7a9193ae56328e5dd6f6aec903d6e7
hbigman0921 <hbigman0921@ajou.ac.kr>
2017-12-11 18:34:01 +0900

    20171211-이인태-수강신청 페이지 작업

--- Course-Register-Assistance-Web-Application/src/app/Sugang.ts
+++ Course-Register-Assistance-Web-Application/src/app/Sugang.ts
@@ -1,6 +1,7 @@
 export class Sugang {
   sugangName: string;
   sugangNumber: string;
+  subjectTime: string;
   constructor(sName: string, sNumber: string) {
     this.sugangName = sName;
     this.sugangNumber = sNumber;
